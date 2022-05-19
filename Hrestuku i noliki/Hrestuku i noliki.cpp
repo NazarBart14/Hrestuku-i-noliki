6548763e9cd2e0e31ede5c5ff6682dd9b74dd595
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2022-05-19 16:41:25 +0300

    Добавьте файлы проекта.

---
+++ Hrestuku i noliki/Hrestuku i noliki.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+
+#include "Player.h"
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	Pole pole;
+	Player player;
+	//pole.printPole(player);
+
+	int x = 1; int y = 1, i = 0;
+	int ship = 10;
+	char ch = 'X';
+	while (true) {
+		char key = _getch();
+
+		if (key == 'w') x--;
+		if (key == 's') x++;
+		if (key == 'a') y--;
+		if (key == 'd') y++;
+		if (x == 0 or x == 10 - 1 or y == 0 or y == 10 - 1) {//умова,за якої при виході за межі поля, змія повертається 
+			x = 2;
+			y = 2;
+		}
+
+
+		if (key == 'c' && i == 0) {
+			ch = 'X';
+			player.setX(x);
+			player.setY(y);
+			player.setCh(ch);
+			system("cls");
+
+			pole.printPole(player);
+			i = 1;
+		}
+		else if (key == 'c' && i == 1) {
+			ch = '0';
+			player.setX(x);
+			player.setY(y);
+			player.setCh(ch);
+			system("cls");
+			pole.printPole(player);
+			i = 0;
+		}
+		
+		else {
+			system("cls");
+			pole.printPole(x, y, '*');
+		}
+
+	}
+	while (ship == 0) {
+		if (ch = 'c')
+		{
+			ship--;
+			cout << ship;
+		}
+	}
+
+}
